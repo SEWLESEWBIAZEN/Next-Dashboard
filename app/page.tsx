d163e675038f09d0392c3c8de723a913f0beb020
Sewlesew Biazen <sewlesewbiazen65@gmail.com>
2024-06-25 10:26:52 +0300

    creating a dashboard link to home

--- app/page.tsx
+++ app/page.tsx
@@ -20,12 +20,20 @@ export default function Page() {
             </a>
             , brought to you by Vercel.
           </p>
+          <div className='flex flex-col lg:flex-row justify-between'>
           <Link
             href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+            className="flex mb-3 md:me-3 items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
             <span>Log in</span> 
           </Link>
+          <Link
+            href="/dashboard"
+            className="flex items-center gap-5 self-start rounded-lg border border-black  px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+          >
+            <span className='text-black '>Dashboard</span> 
+          </Link>
+          </div>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
