1da70d361f283d285aea2a979dbb023562e205b1
Sewlesew Biazen <sewlesewbiazen65@gmail.com>
2024-06-26 08:05:23 +0300

    extracting form data and assign to new formdata object

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,13 @@
 'use server'
 
 export async function createInvoice(formData:FormData){
-    
+    const rawData={
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    };
+
+    //test
+    console.log(rawData);
+
 }
