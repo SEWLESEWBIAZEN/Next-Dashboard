647c5d32de27577245bb9303be133878d396a140
Sewlesew Biazen <sewlesewbiazen65@gmail.com>
2024-06-26 08:15:18 +0300

    adding validation library zod

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,13 +1,24 @@
 'use server'
+import {z} from 'zod';
 
+const FormSchema= z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.number(),
+    status: z.enum(['pending','paid']),
+    date: z.string(),
+})
+
+const CreateInvoice=FormSchema.omit({id:true, date:true})
 export async function createInvoice(formData:FormData){
-    const rawData={
+    const {customerId,amount,status}= CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
-    };
+    });
+    // const rawFormData = Object.fromEntries(formData.entries())
 
     //test
-    console.log(rawData);
+   
 
 }
