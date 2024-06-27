d4bc12f41fbbdc81142a1a5b24882c00bb937459
Sewlesew Biazen <sewlesewbiazen65@gmail.com>
2024-06-27 14:09:42 +0300

    type error solved

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -25,7 +25,7 @@ const FormSchema = z.object({
 
 export type State={
     errors?:{
-        customrrId?:string[];
+        customerId?:string[];
         amount?:string[];
         status?:string[];
         
