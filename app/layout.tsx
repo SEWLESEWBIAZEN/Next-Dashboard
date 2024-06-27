93ffc8807ad0bd1cd2e268d9faa3100c1b288dfc
Sewlesew Biazen <sewlesewbiazen65@gmail.com>
2024-06-27 11:48:12 +0300

    adding a metadata to the app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
 import '@/app/ui/global.css'
 import { inter } from './ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata:Metadata={
+  title:'Acme Dashboard',
+  description:'The First Next.js Project I made using App Router',
+  metadataBase: new URL('https://sewdevs.netlify.app')
+}
 export default function RootLayout({
   children,
 }: {
