92b19fcd6747f6bae5e651b7d79b4bb2791febb3
Sewlesew Biazen <sewlesewbiazen65@gmail.com>
2024-06-27 14:47:19 +0300

    undoe

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,10 @@
 import NextAuth from 'next-auth';
 import { AuthConfig } from './app/auth.config';
  
- export default NextAuth(AuthConfig).auth;
-
- 
+ export default NextAuth(AuthConfig).auth; 
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 };
+
+// 
