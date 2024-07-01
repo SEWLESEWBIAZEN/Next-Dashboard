834befd4060018e7cef382b590465cf0e46265a4
Sewlesew Biazen <sewlesewbiazen65@gmail.com>
2024-07-01 10:39:40 +0300

    creating the user application they intra

---
+++ e-commerce/page.jsx
@@ -0,0 +1,12 @@
+// this the customer page that they are going to interact with
+
+
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
