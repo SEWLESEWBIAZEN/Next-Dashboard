9c4c311ffb2c1760c0b7e330365ac256a52b4b99
Sewlesew Biazen <sewlesewbiazen65@gmail.com>
2024-07-01 16:50:47 +0300

    assigning class names to icons and logo

--- app/e-commerce/_components/HeaderMain.jsx
+++ app/e-commerce/_components/HeaderMain.jsx
@@ -12,7 +12,7 @@ const HeaderMain = () => {
             <div className='container py-6'>
                 <div className='flex flex-col sm:flex-row justify-between items-center gap-4'>
                     {/* brand name/ logo */}
-                    <div className='text-[18px] hidden sm:block'><strong>Ac</strong>ME</div>
+                    <div className='brand text-[18px] hidden sm:block'><strong>Ac</strong>ME</div>
 
                     {/* search bar */}
                     <div className='flex flex-row items-center w-[300px] sm:w-[70%] text-gray-400 -space-x-5'>
@@ -30,13 +30,13 @@ const HeaderMain = () => {
                     <div className=' hidden sm:flex flex-row gap-2'>
                         <div><UserCircleIcon className='h-6 w-6' /></div>
                         <div className='-space-y-9 '>
-                            <HeartIcon className='h-6 w-6 z-0 hover:text-blue-500'  onClick={()=>setCountSaved(countSaved+1)}/>
-                            <p className='text-center grid translate-x-1 -translate-y-1 items-center text-[16px] rounded rounded-full bg-blue-500 z-40'>{countSaved}</p>
+                            <HeartIcon className='h-6 w-6 z-0 icon'  onClick={()=>setCountSaved(countSaved+1)}/>
+                            <p className='text-center grid translate-x-1 -translate-y-1 items-center text-[16px] rounded rounded-full badge z-40'>{countSaved}</p>
 
                         </div>
                         <div className='-space-y-9'>
-                            <ShoppingBagIcon className='h-6 w-6 z-0 hover:text-blue-500' onClick={()=>setCountPurchased(countPurchased+1)} />
-                            <p className='text-center grid translate-x-1 -translate-y-1 items-center text-[16px] rounded rounded-full bg-blue-500 z-40'>{countPurchased}</p>
+                            <ShoppingBagIcon className='h-6 w-6 z-0 icon' onClick={()=>setCountPurchased(countPurchased+1)} />
+                            <p className='text-center grid translate-x-1 -translate-y-1 items-center text-[16px] rounded rounded-full badge z-40'>{countPurchased}</p>
 
                         </div>
 
